753df5366685dbd41a158354f47a51be43b54def
horos-cope <swa1735@kookmin.ac.kr>
2020-06-28 04:32:28 +0900

    issue #4 WSGI: 두 수의 합과 곱

---
+++ calc/template.py
@@ -0,0 +1,11 @@
+html = b"""
+<html>
+    <body>
+        <form action="">
+            a = <input type="number" name = "a"><br>
+            b = <input type="number" name = "b"><br><br>
+            <input type="submit">
+        </form>
+        #저장한 add와 mul을 출력
+    </body>
+</html>
